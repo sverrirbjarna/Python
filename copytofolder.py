8c5577ef1980cbe2d90f4cf30e1a1314b3ef0a6e
sverrirbjarna <sverrirbjarnason@gmail.com>
2020-12-12 13:04:53 +0000

    nyo

---
+++ copytofolder.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+my_file = open("/home/sverrir/Documents/Yolo_data/Biersdorf_split/Joined/Biersdorf_all/test.txt", "r")
+content_list = my_file.read().splitlines()
+
+print(content_list)
+
+for f in content_list:
+    shutil.copy(f, '/home/sverrir/Documents/Yolo_data/Biersdorf_split/Joined/test10pall')
